0bc10e845d567890dad03b8dce2ca4b1a7981364
locknew <bangbang12353@gmail.com>
2022-07-22 01:11:18 +0700

    4.12 fix

---
+++ src/components/ExCard.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="Ex-card">
+    <span>@{{ event.category }}</span>
+    <h3>{{ event.organizer }}</h3>
+  </div>
+</template>
+<script>
+export default {
+  name: 'ExCard',
+  props: {
+    event: {
+      type: Object,
+      require: true
+    }
+  }
+}
+</script>
+
+<style scoped>
+.Ex-card {
+  padding: 20px;
+  width: 250px;
+  cursor: pointer;
+  border: 1px solid #39495c;
+  margin-bottom: 18px;
+}
+
+.Ex-card:hover {
+  transform: scale(1.01);
+  box-shadow: 0 3px 12px 0 rgba(0, 0, 0, 0.2);
+}
+</style>
